64f4f02660f934a648fb7c72bbc9de79220177d4
Alessandro Belli <alessandrobelli90@gmail.com>
2021-10-25 17:13:34 +0200

    new color set

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,30 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: '#6161F0',
+      red: colors.rose,
+      yellow: '#F0F061',
+      green: '#61F0A9',
+      pink: '#F061A8'
+    },
     extend: {
       colors: {
-        primary: '#61F0A9',
-        primarydarker: '#14d776',
-        primarydarkerdarker: '#0b7b43',
-        secondary: '#F26B38',
-        secondarydarker: '#c33e0c',
-        secondarydarkerdarker: '#6f2307',
         graySecondary: '#A2A2A6',
-        grayPrimary: '#28282F'
+        grayPrimary: '#28282F',
+        primary: '#61F0A9',
+        myblue: '#6161F0',
+        mypink: '#F061A8',
+        myyellow: '#F0F061'
 
       },
       fontFamily:{
